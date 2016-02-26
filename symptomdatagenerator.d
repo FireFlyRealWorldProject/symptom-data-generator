1b9887bb71bbc0d384aa52a0197dc3461fe128de
Sam Barratt <samathy@sbarratt.co.uk>
2016-02-26 23:56:43 +0000

    Added reading reporting methods and locations from file

--- symptomdatagenerator.d
+++ symptomdatagenerator.d
@@ -20,13 +20,17 @@ void main(string[] args)
     File ids;
     File Asymptoms;
     File Osymptoms;
+    File ReportMethods;
+    File LocationPairs;
 
-    if (args.length >= 6)       //Open the files
+    if (args.length >= 8)       //Open the files
     {
         JSON = File(args[1], "r");
         ids = File(args[2], "r");
-        Asymptoms = File(args[3], "r");
+        Asymptoms = file(args[3], "r");
         Osymptoms= File(args[4], "r");
+        ReportMethods =  File(args[5], "r");
+        LocationPairs = File(args[6], "r");
     }
     else
     {
@@ -63,6 +67,9 @@ void main(string[] args)
 
     //XXX For now we have statically defined limits to how many symptoms there are - We could make this dynamic
 
+
+    //TODO Put all this inside a function - Its horrible out here!
+
     string AsymptomsList[];
     AsymptomsList.length = 20;
     lineCount = 0;
@@ -83,6 +90,26 @@ void main(string[] args)
         lineCount++;
     }
 
+    string ReportMethodsList[];
+    ReportMethodsList.length = 20;
+    lineCount = 0;
+    while(!ReportMethods.eof())
+    {
+        string line = strip (ReportMethods.readln());
+        ReportMethodsList[lineCount] = line;
+        lineCount++;
+    }
+
+    string LocationPairsList[];
+    LocationPairsList.length = 20;
+    lineCount = 0;
+    while(!LocationPairs.eof())
+    {
+        string line = strip (LocationPairs.readln());
+        LocationPairsList[lineCount] = line;
+        lineCount++;
+    }
+
     writefln("The asymptoms list is %s long", AsymptomsList.length);
     writefln("The osymptoms list is %s long", OsymptomsList.length);
 
@@ -92,11 +119,9 @@ void main(string[] args)
     while (!ids.eof())  //For every ID
     {
         patiants[i] = parseJSON(JSONStructureString); //New patiant
-        for (int i = 0; i < 10; i++)
-        {
-            patiants[i].object[JSONStructureFields[i]] = JSONValue(ids.readln());
+        patiants[i].object["patiant_id"] = JSONValue(ids.readln());
 
-        }
+
 
     }
         
